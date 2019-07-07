aaea488427fe2fb895cf056984ec934afb350314
Nap Joseph Calub <njncalub@gmail.com>
2019-07-07 13:21:42 +0800

    Created new apps for wallets and transactions.

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,7 +79,8 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "project.users.apps.UsersConfig",
-    # Your stuff: custom apps go here
+    "project.wallets.apps.WalletsConfig",
+    "project.transactions.apps.TransactionsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

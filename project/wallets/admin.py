38d3e2713814865b8200a54275d54bacf68703e9
Nap Joseph Calub <njncalub@gmail.com>
2019-07-08 21:20:24 +0800

    Added Wallet model and displayed it in the admin page.

--- project/wallets/admin.py
+++ project/wallets/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Wallet
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "initial_balance",
+        "currency",
+        "created_by",
+        "date_created",
+        "is_active",
+    )
+    list_filter = ("is_active",)

ff6a859ef2802834f9718b27c55aa6b5b43401af
Nap Joseph Calub <njncalub@gmail.com>
2019-07-13 22:14:12 +0800

    Added StandardCategory model.
    Also added kinds to categories:
    
    * DEBIT = Income accounts
    * CREDIT = Expense accounts

--- project/categories/admin.py
+++ project/categories/admin.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, StandardCategory
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ["name", "wallet__name"]
+    raw_id_fields = ("parent", "wallet", "created_by")
     list_display = (
         "name",
+        "kind",
         "wallet",
         "full_path",
         "created_by",
         "date_created",
         "is_active",
     )
-    list_filter = ("date_created", "is_active")
+    list_filter = ("kind", "date_created", "is_active")
+
+
+@admin.register(StandardCategory)
+class StandardCategoryAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    raw_id_fields = ("parent", "created_by")
+    list_display = ("name", "kind", "full_path", "created_by", "date_created")
+    list_filter = ("kind", "date_created")
--- project/categories/models.py
+++ project/categories/models.py
@@ -5,11 +5,24 @@ from django.conf import settings
 
 from project.wallets.models import Wallet
 
+# TODO(njncalub): Move to a separate constants file or use a different Model.
+CATEGORY_KIND_DEBIT = "DR"
+CATEGORY_KIND_CREDIT = "CR"
+CATEGORY_KIND_CHOICES = [
+    # Debit categories add money by default (income accounts).
+    (CATEGORY_KIND_DEBIT, "INCOME"),
+    # Credit categories subtract money by default (expense accounts).
+    (CATEGORY_KIND_CREDIT, "EXPENSE"),
+]
+
 
 class Category(models.Model):
     """A way to categorize and group together Transactions."""
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    kind = models.CharField(
+        max_length=2, choices=CATEGORY_KIND_CHOICES, default=CATEGORY_KIND_CREDIT
+    )
     wallet = models.ForeignKey(
         Wallet,
         blank=True,
@@ -34,6 +47,47 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.full_path
+
+    @property
+    def full_path(self):
+        paths = [self.name]
+
+        p = self.parent
+        while p is not None:
+            paths.append(p.name)
+            p = p.parent
+
+        return " -> ".join(paths[::-1])
+
+
+class StandardCategory(models.Model):
+    """Default categories that will be added to new Wallets."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    kind = models.CharField(
+        max_length=2, choices=CATEGORY_KIND_CHOICES, default=CATEGORY_KIND_CREDIT
+    )
+    name = models.CharField(max_length=25)
+    parent = models.ForeignKey(
+        "self",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="children",
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="standard_categories",
+    )
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "standard categories"
 
     def __str__(self):
         return self.full_path
